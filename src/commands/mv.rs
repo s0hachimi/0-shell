c7c0c7c54aaaa82fe526f3a079dc177524d22ee3
Ajebbari <jebbari635@gmail.com>
2025-09-18 18:13:54 +0100

    fixing mv

--- src/commands/mv.rs
+++ src/commands/mv.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::path::{Path, PathBuf};
 
 pub fn mv(args: Vec<&str>) {
     if args.len() != 2 {
@@ -8,8 +9,19 @@ pub fn mv(args: Vec<&str>) {
 
     let source = args[0];
     let dest = args[1];
+    let dest_path = Path::new(dest);
+    let source_filename = Path::new(source)
+        .file_name()
+        .unwrap_or_default();
 
-    if let Err(e) = fs::rename(source, dest) {
-        eprintln!("mv: cannot move '{}' to '{}': {}", source, dest, e);
+    let is_dir = dest_path.is_dir() || dest.ends_with('/') || dest.ends_with("\\");
+    let final_dest: PathBuf = if is_dir {
+        dest_path.join(source_filename)
+    } else {
+        dest_path.to_path_buf()
+    };
+
+    if let Err(e) = fs::rename(source, &final_dest) {
+        eprintln!("mv: cannot move '{}' to '{}': {}", source, final_dest.display(), e);
     }
 }
